af8b7c88e3bbd2f69bbbe7a0ebb1d4c7e2628630
o.honchar <o.honchar@bvblogic.com>
2018-01-25 17:25:02 +0200

    Edit footer, contacts social icons

--- wp-develop/wp-content/themes/astamart/footer.php
+++ wp-develop/wp-content/themes/astamart/footer.php
@@ -11,25 +11,44 @@
 
 ?>
 
-	</div><!-- .tm-content-wrap End -->
+</div><!-- .tm-content-wrap End -->
 
 <div class="tm-content-wrap"><!-- Copyright Start -->
     <div id="copy">
         <div class="tm-global-wrap uk-grid">
+            <div id="footer-service" class="tm-global-wrap-right uk-width-large-1-4 uk-width-medium-3-10 uk-width-small-1-1">
+                <h2>Услуги и сервис</h2>
+                <div class="tm-content-full">
+                    <ul>
+                        <li><a href="<?php echo get_site_url();?>#constructor" class="uk-animation-scale-down">Конструктор картин</a></li>
+                        <li><a href="/production-of-paintings/">Производство картин</a></li>
+                        <li><a href="/pay-and-delivery/">Доставка и оплата</a></li>
+                        <li><a href="/types-edges-pictures/">Типы кромок картин</a></li>
+                        <li><a href="/sitemap.xml">Карта сайта</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="footer-client"  class="tm-global-wrap-left uk-width-large-3-4 uk-width-medium-7-10 uk-width-small-1-1">
+                <h2>Клиенту</h2>
+                <div class="tm-content-full">
+                    <ul>
+                        <li><a href="/cost-pictures/">Стоимость картин</a></li>
+                        <li><a href="/action/">Акции</a></li>
+                        <li><a href="/gallary/">Галерея </a></li>
+                        <li><a href="/articles/">Статьи</a></li>
+                        <li><a href="/reviews/">Отзывы</a></li>
+                    </ul>
+                </div>
+        </div>
             <div class="tm-global-wrap uk-width-large-1-1 uk-width-medium-1-1 uk-width-small-1-1 uk-text-center">
                 <img src="https://counter.yadro.ru/hit?t52.6;rhttp%3A//www.liveinternet.ru/rating/ru/computers/;s1600*900*24;uhttps%3A//losst.ru/;0.6502722554840501">
                 <img src="https://counter.yadro.ru/hit?t52.6;rhttp%3A//www.liveinternet.ru/rating/ru/computers/;s1600*900*24;uhttps%3A//losst.ru/;0.6502722554840501">
                 <img src="https://counter.yadro.ru/hit?t52.6;rhttp%3A//www.liveinternet.ru/rating/ru/computers/;s1600*900*24;uhttps%3A//losst.ru/;0.6502722554840501">
             </div>
-        </div>
-        <div class="tm-global-wrap uk-width-large-1-1 uk-width-medium-1-1 uk-width-small-1-1">
-            <div class="tm-content-full uk-text-center">
-                <a href="http://astamart.ru/sitemap.xml" title="sitemap">Карта сайта</a>
-            </div>
-        </div>
-        <div class="tm-global-wrap uk-width-large-1-1 uk-width-medium-1-1 uk-width-small-1-1">
-            <div class="tm-content-full uk-text-center">
-                ©  ASTA M. Модульные картины. 2017. Все права защищены.
+            <div class="tm-global-wrap uk-width-large-1-1 uk-width-medium-1-1 uk-width-small-1-1">
+                <div class="tm-content-full uk-text-center">
+                    © ASTA M. Модульные картины. 2017. Все права защищены.
+                </div>
             </div>
         </div>
     </div>
--- wp-develop/wp-content/themes/astamart/inc/front-page/contacts.php
+++ wp-develop/wp-content/themes/astamart/inc/front-page/contacts.php
@@ -11,10 +11,9 @@
 		</div>
         <div class="tm-cont-block uk-width-large-1-1 uk-width-medium-1-1 uk-width-small-1-1">
             <div id="social-icons">
-                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/social-icons/fb.png'?>;" alt="facebook"></a>
-                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/social-icons/vk.png'?>;" alt="vk"></a>
-                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/social-icons/ok.png'?>;" alt="ok"></a>
-                <a href="#"><img src="<?php echo get_template_directory_uri() . '/images/social-icons/youtube.png'?>;" alt="youtube"></a>
+                <a href="https://www.facebook.com/astamanufactura"><img src="<?php echo get_template_directory_uri() . '/images/social-icons/fb.png'?>;" alt="facebook"></a>
+                <a href="https://vk.com/astamanufactura "><img src="<?php echo get_template_directory_uri() . '/images/social-icons/vk.png'?>;" alt="vk"></a>
+                <a href="https://www.youtube.com/channel/UCnK8NajsjZJ9O_R6b-oWC8g?sub_confirmation=1"><img src="<?php echo get_template_directory_uri() . '/images/social-icons/youtube.png'?>;" alt="youtube"></a>
             </div>
         </div>
 	</div>
